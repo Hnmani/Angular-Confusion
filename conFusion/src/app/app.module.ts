c17d37bd2298a0d56199df5d7191621726124a29
Hnmani <harshbhumihar@gmail.com>
2020-01-17 00:23:53 +0530

    about us done

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +46,9 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule,
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
