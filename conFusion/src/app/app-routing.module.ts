9d9dc8af848272697687010559c4bf2fdb8bf225
Hnmani <harshbhumihar@gmail.com>
2020-01-16 23:03:22 +0530

    Angular Router Basics

--- conFusion/src/app/app-routing.module.ts
+++ conFusion/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { routes } from './shared/routes';
 
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations:[]
+
 })
 export class AppRoutingModule { }
